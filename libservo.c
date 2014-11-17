94d60eb5cee154412640378e780c7e28e9e12b60
Kevin Zheng <kevinz5000@gmail.com>
2014-11-17 17:10:11 -0600

    Document servo library

--- libservo.c
+++ libservo.c
@@ -1,18 +1,33 @@
+/**
+ * @file
+ * Servo control interface for common use cases
+ */
+
 typedef struct {
     int port;
     int p_open, p_close;
 } servo_s;
 
-void servo_init(servo_s *servo, int port, int up, int down) {
+/**
+ * Initialize the given servo structure with the given port number, open
+ * setting, and closed setting.
+ */
+void servo_init(servo_s *servo, int port, int open, int closed) {
     servo->port = port;
-    servo->p_open = up;
-    servo->p_close = down;
+    servo->p_open = open;
+    servo->p_close = closed;
 }
 
+/**
+ * Set the given servo to its open setting.
+ */
 void servo_open(servo_s *servi) {
     servo[servi->port] = servi->p_open;
 }
 
+/**
+ * Set the given servo to its closed setting.
+ */
 void servo_close(servo_s *servi) {
     servo[servi->port] = servi->p_close;
 }
