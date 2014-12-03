016c3625bd0912b0e2306339bdc0ab4cef312eb8
Kevin Zheng <kevinz5000@gmail.com>
2014-12-03 15:43:09 -0600

    Add autonomous configuration menu

--- 407/autonomous.c
+++ 407/autonomous.c
@@ -16,6 +16,92 @@
 
 #include "common.c"
 
+typedef enum {
+    CONFIG_RAMP, CONFIG_DELAY, CONFIG_END
+} option_keys;
+
+const string config_names[CONFIG_END] = {
+    "Ramp",
+    "Delay",
+};
+
+const int config_levels[CONFIG_END][] = {
+    {0, 1, -1, 0, 0},
+    {0, 5, 10, 15, -1},
+};
+
+int config[CONFIG_END];
+
+int config_get(option_keys option) {
+    return config_levels[option][config[option]];
+}
+
+/**
+ * Count the number of configuration levels for each given option.
+ */
+static int config_count(int option) {
+    for (int i = 0; true; i++) {
+        if (config_levels[option][i] == -1) {
+            return i;
+        }
+    }
+}
+
+/**
+ * Display menu with autonomous program configuration options.
+ */
+static void config_menu() {
+    // Manually handle button actions.
+    nNxtButtonTask = -2;
+    int state = 0, selection = 0;
+
+    while (true) {
+        int numbopts = config_count(state);
+        int button;
+
+        // Redraw configuration menu.
+        eraseDisplay();
+        nxtDisplayTextLine(0, "%s < %d >", config_names[state], config_levels[state][selection]);
+
+        // Wait for a button press.
+        while (true) {
+            button = nNxtButtonPressed;
+            if (button != kNoButton) {
+                break;
+            }
+            Sleep(100);
+        }
+
+        // Process button press.
+        switch (button) {
+            case kLeftButton:
+                if (selection - 1 >= 0) {
+                    selection -= 1;
+                }
+                break;
+            case kRightButton:
+                if (selection + 1 < numbopts) {
+                    selection += 1;
+                }
+                break;
+            case kEnterButton:
+                config[state] = selection;
+                selection = 0;
+                state++;
+                if (state == (short)CONFIG_END) {
+                    eraseDisplay();
+                    return;
+                }
+                break;
+            default:
+                StopAllTasks();
+                break;
+        }
+
+        Sleep(250);
+    }
+}
+
 void init() {
     init_common();
     gyro_init(gyro, false);
@@ -62,8 +148,16 @@ void start_ramp() {
 }
 
 task main() {
+    config_menu();
+
     waitForStart();
     init();
 
-    start_ramp();
+    Sleep(config_get(CONFIG_DELAY) * 1000);
+
+    if (config_get(CONFIG_RAMP)) {
+        start_ramp();
+    } else {
+        start_ground();
+    }
 }
