6af84010a8616763a3bda24bdbbca730a10ac4c4
Kevin Zheng <kevinz5000@gmail.com>
2015-01-24 10:15:35 -0600

    Update battery sanity check

--- lib/test.c
+++ lib/test.c
@@ -1,11 +1,4 @@
 /**
- * @file
- * Status checks and unit testing for libraries
- */
-
-#include "gyro.c"
-
-/**
  * Return a random number between min and max, inclusive.
  */
 int rand_int(int min, int max) {
@@ -16,11 +9,15 @@ int rand_int(int min, int max) {
  * Return false if a potential battery problem is detected.
  */
 bool check_battery() {
-    if (externalBatteryAvg < 12 * 1000 || nAvgBatteryLevel < 6 * 1000) {
+    if (externalBatteryAvg < 12 * 1000) {
+        writeDebugStreamLine("[check_battery] External battery");
         return false;
+    } else if (nAvgBatteryLevel < 6 * 1000) {
+        writeDebugStreamLine("[check_battery] NXT battery");
+        return false;
+    } else {
+        return true;
     }
-
-    return true;
 }
 
 /**
@@ -41,18 +38,4 @@ task check() {
  */
 void check_init() {
     startTask(check);
-}
-
-bool test_gyro() {
-    for (int i = 0; i < 20; i++) {
-        int angle = 90 * rand_int(1, 6) * sgn(rand_int(0, 10) - 5);
-        gyro_turn(angle, rand_int(40, 60));
-
-        writeDebugStream("Case %d: %d out of %d\n", i + 1, gyro_heading(), angle);
-        playSound(soundBeepBeep);
-    }
-
-    writeDebugStream("===>>> Passed gyro test!");
-    playSound(soundUpwardTones);
-    return true;
 }
