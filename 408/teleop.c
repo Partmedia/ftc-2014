e11b9f7b7e1af5f1703465ada216bc73c95eaeaf
Robbie Wadud <waduda3835@parkwayschools.net>
2015-01-26 15:35:13 -0600

    408: Rename servo ports

--- 408/teleop.c
+++ 408/teleop.c
@@ -1,12 +1,14 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
 #pragma config(Hubs,  S2, HTServo,  none,     none,     none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
 #pragma config(Motor,  mtr_S1_C1_1,     motorD,        tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C1_2,     m_right,       tmotorTetrix, openLoop, reversed)
 #pragma config(Motor,  mtr_S1_C2_1,     m_left,        tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C2_2,     lift,          tmotorTetrix, openLoop, reversed)
 #pragma config(Servo,  srvo_S2_C1_1,    gate,                 tServoStandard)
-#pragma config(Servo,  srvo_S2_C1_2,    grabber_r,            tServoStandard)
-#pragma config(Servo,  srvo_S2_C1_3,    grabber_l,            tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_2,    sp_grabber_r,         tServoStandard)
+#pragma config(Servo,  srvo_S2_C1_3,    sp_grabber_l,         tServoStandard)
 #pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
 #pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
 #pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
@@ -38,11 +40,11 @@ task main() {
 
         //Grabber
         if (joy1Btn(6)) {
-	        servo[grabber_l] = 220; //up
-            servo[grabber_r] = 35;
+	        servo[sp_grabber_l] = 220; //up
+            servo[sp_grabber_r] = 35;
         } else if (joy1Btn(8)) {
-            servo[grabber_l] = 80;  //down
-            servo[grabber_r] = 175;
+            servo[sp_grabber_l] = 80;  //down
+            servo[sp_grabber_r] = 175;
         }
 
         // Gate
