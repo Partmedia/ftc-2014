184e03213ede4e93aa5f3b93c0a29b2db504f6de
Kevin Zheng <kevinz5000@gmail.com>
2015-01-29 17:36:54 -0600

    407: Add work-in-progress IR routine

--- 407/ir.c
+++ 407/ir.c
@@ -1,4 +1,33 @@
+#include "../lib/drive.c"
+#include "../lib/gyro.c"
+#include "../lib/ir.c"
+
 const int pdrive = 40, pturn = 70;
+
+/**
+ * Initialize the robot and drop into an infinite loop to debug IR.
+ */
+void ir_debug() {
+    while (true) {
+        ir_update();
+        writeDebugStreamLine("[ir_debug] %d, %d",
+                ir_left.acDirection, ir_right.acDirection);
+        sleep(1000);
+    }
+}
+
+int ir_position() {
+    ir_update();
+    if (ir_left.acDirection == 3) {
+        return 3;
+    } else if (ir_left.acDirection == 5) {
+        return 2;
+    } else if (ir_left.acDirection == 0) {
+        return 1;
+    } else {
+        writeDebugStreamLine("[ir_position] Unknown position!");
+    }
+}
 
 /**
  * Pre-position the robot so that it is ready for final approach towards the
@@ -6,6 +35,18 @@ const int pdrive = 40, pturn = 70;
  */
 void auto_ir_prepos() {
     drive_straight(pdrive, 500);
+    gyro_turn_abs(-45);
+    drive_straight(pdrive, 1000);
+    gyro_turn_abs(45);
+    drive_straight(pdrive, 300);
+
+    ir_debug();
+
+    while (true) {
+        writeDebugStreamLine("[auto_ir_prepos] Position: %d", ir_position());
+        sleep(1000);
+    }
+
     if (ir_dual_ready_final()) {
         writeDebugStreamLine("[auto_ir_prepos] Straight ahead!");
         auto_ir_dual_final(pdrive, pturn);
@@ -14,18 +55,13 @@ void auto_ir_prepos() {
          writeDebugStreamLine("[auto_ir_prepos] Not straight ahead...");
     }
 
-    // Move towards the left of the field.
-    gyro_turn_abs(-45, pturn);
-    drive_straight(pdrive, 1100);
-    gyro_turn_abs(0, pturn);
-
     // Move forwards until we detect IR beacon perpendicularly.
     //while (SensorValue[ir_right] <= 7) {
     //    drive_straight(pdrive, 250);
     //}
 
     // Check if IR beacon is in second position.
-    gyro_turn_abs(45, pturn);
+    gyro_turn_abs(45);
     if (ir_dual_ready_final()) {
         writeDebugStreamLine("[auto_ir_prepos] Position 2!");
         auto_ir_dual_final(pdrive, pturn);
@@ -35,7 +71,7 @@ void auto_ir_prepos() {
     }
 
     // Move to third center goal position.
-    gyro_turn_abs(0, pturn);
+    gyro_turn_abs(0);
     drive_straight(pdrive, 1000);
 }
 
@@ -47,17 +83,4 @@ void auto_centergoal() {
     auto_ir_prepos();
     auto_ir_dual_final(pdrive, pturn);
     writeDebugStreamLine("[auto_centergoal] At center goal (I hope)");
-}
-
-/**
- * Initialize the robot and drop into an infinite loop to debug IR.
- */
-void ir_debug() {
-    init();
-    while (true) {
-        ir_update();
-        writeDebugStreamLine("[ir_debug] %d, %d",
-                ir_left.acDirection, ir_right.acDirection);
-        sleep(1000);
-    }
 }
