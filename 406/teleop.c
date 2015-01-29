75e2fa2d36eadf337175659106fa740315fa1bed
Townshend Miransky <townshend.miransky@gmail.com>
2015-01-29 15:41:15 -0600

    406: Change grabber rotation

--- 406/teleop.c
+++ 406/teleop.c
@@ -1,15 +1,15 @@
 #pragma config(Hubs,  S4, HTMotor,  HTMotor,  HTMotor,  HTServo)
-#pragma config(Sensor, S4,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S1,     IR,             sensorHiTechnicIRSeeker1200)
 #pragma config(Motor,  motorA,          winder,        tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  motorB,          NA,            tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  motorC,          NA,            tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  mtr_S4_C1_1,     Squid,         tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C1_2,     Lever,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C2_1,     Rack,          tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C2_2,     motorE,        tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C3_1,     Left,          tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S4_C3_2,     Right,         tmotorTetrix, openLoop)
-#pragma config(Servo,  srvo_S4_C4_1,    Grabber,              tServoNone)
+#pragma config(Motor,  mtr_S4_C2_1,     Left,          tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S4_C2_2,     Right,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C3_1,     Rack,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C3_2,     MotorE,        tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S4_C4_1,    Grabber,              tServoStandard)
 #pragma config(Servo,  srvo_S4_C4_2,    servo2,               tServoNone)
 #pragma config(Servo,  srvo_S4_C4_3,    servo3,               tServoNone)
 #pragma config(Servo,  srvo_S4_C4_4,    servo4,               tServoNone)
@@ -51,10 +51,10 @@ task main() {
 
         // Servo controls (moveable goal grabbers). //
         if (joy1Btn(5)) {
-            servo[Grabber] = 180;
+            servo[Grabber] = 270;
         }
         if (joy1Btn(7)) {
-            servo[Grabber] = -180;
+            servo[Grabber] = -270;
         }
         if (joy1Btn(6)) {
         		motor[Left] = 20;
