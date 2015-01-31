de30d66c5a4f7d499d8dc0f99c0ace9ea8ca00be
Townshend Miransky <townshend.miransky@gmail.com>
2015-01-31 12:04:39 -0600

    406: Rewire drive motors and adjust lever power

--- 406/teleop.c
+++ 406/teleop.c
@@ -1,14 +1,15 @@
 #pragma config(Hubs,  S4, HTMotor,  HTMotor,  HTMotor,  HTServo)
 #pragma config(Sensor, S1,     IR,             sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S4,     ,               sensorI2CMuxController)
 #pragma config(Motor,  motorA,          winder,        tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  motorB,          NA,            tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  motorC,          NA,            tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  mtr_S4_C1_1,     Squid,         tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C1_2,     Lever,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C2_1,     Left,          tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S4_C2_1,     MotorB,        tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C2_2,     Right,         tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C3_1,     Rack,          tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S4_C3_2,     MotorE,        tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S4_C3_2,     Left,          tmotorTetrix, openLoop, reversed)
 #pragma config(Servo,  srvo_S4_C4_1,    Grabber,              tServoStandard)
 #pragma config(Servo,  srvo_S4_C4_2,    servo2,               tServoNone)
 #pragma config(Servo,  srvo_S4_C4_3,    servo3,               tServoNone)
@@ -31,7 +32,7 @@ task main() {
         getJoystickSettings(joystick);
         drive_handle_joystick();
         motor[Rack] = joystick.joy2_y1;
-        motor[Lever] = joystick.joy2_y2 * 0.15;
+        motor[Lever] = joystick.joy2_y2 * 0.3;
 
         if (joy2Btn(6)) {
             motor[Squid] = 50;
