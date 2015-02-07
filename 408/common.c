1a6d5e8665dee786b1effe4d3b9dafbb350efd12
Kevin Zheng <kevinz5000@gmail.com>
2015-02-06 18:05:06 -0600

    408: Refactor autonomous and teleop

---
+++ 408/common.c
@@ -0,0 +1,20 @@
+servo_s servo_right, servo_left, servo_gate;
+
+void grabber_up() {
+    servo_open(&servo_left);
+    servo_open(&servo_right);
+}
+
+void grabber_down() {
+    servo_close(&servo_left);
+    servo_close(&servo_right);
+}
+
+void init() {
+    drive_init(m_left, m_right);
+    servo_init(&servo_right, sp_grabber_r, 20, 165);
+    servo_init(&servo_left, sp_grabber_l, 200, 60);
+    servo_init(&servo_gate, gate, 80, 200);
+    grabber_up();
+    servo[gate] = 200;
+}
